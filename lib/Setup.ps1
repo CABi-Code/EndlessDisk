66ca80d6583fbf0f77e64fdf302109af11a1b2f8
CABi <48521225+CABi-Code@users.noreply.github.com>
2026-04-04 02:14:06 +0700

    Rename VBS file from VKDiskMenu.vbs to EndlessDisk.vbs

--- lib/Setup.ps1
+++ lib/Setup.ps1
@@ -19,7 +19,7 @@ objShell.Run cmd, 0, False
 
 function Get-VbsPath {
     $scriptDir = Split-Path -Parent $PSCommandPath
-    return Join-Path $scriptDir "VKDiskMenu.vbs"
+    return Join-Path $scriptDir "EndlessDisk.vbs"
 }
 
 function Ensure-VbsLauncher {
